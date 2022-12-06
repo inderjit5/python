72ac1c8d5feb4158048e8b05480a3c3a5ce1f82e
inderjit5 <114800326+inderjit5@users.noreply.github.com>
2022-12-06 19:27:41 +0530

    Add files via upload

---
+++ ifElse.py
@@ -0,0 +1,8 @@
+#take values of lenght and breadth from user
+L = float(input("let lenght be x"))
+B = float(input("let breadth be y"))
+#print if it is squre or not 
+if L==B
+print("if it is squre")
+else:
+    print("else it is not squre")
